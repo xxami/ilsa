8114e401d49764098cfbeb2b88734eef24c11f4d
lilah <lilah.willacott@gmail.com>
2017-01-09 21:25:11 +0000

    add some init boilerplates!

--- ilsa.py
+++ ilsa.py
@@ -1,5 +1,20 @@
 
 import sys
 
+class Ilsa():
+
+    def __init__(self):
+        pass
+
+    def assembler(self):
+        pass
+
+
+def usage():
+    print('useage: ilsa.py file.ils')
+
 if __name__ == '__main__':
-    if len()
+    if len(sys.argv) > 1:
+        Ilsa(sys.argv).assemble()
+    else:
+        usage()
